665cd8c92ed14c62cde80426799bde4965ad5b04
Michael Komarov <michaelkomarov03@gmail.com>
2023-10-27 20:06:17 +0300

    New image path's calculation system

--- src/main/java/org/apexlegendsbphelper/Main.java
+++ src/main/java/org/apexlegendsbphelper/Main.java
@@ -3,6 +3,7 @@ import static org.apexlegendsbphelper.ImageUtil.*;
 
 import net.sourceforge.tess4j.TesseractException;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -10,46 +11,48 @@ import java.io.IOException;
 
 
 public class Main {
-    public static String folderPath;
-    public static String imagePath = "C:\\Users\\LFKom\\Downloads\\image_2023-10-24_19-32-46.png";
+    public static String inputImagePath = "C:\\Users\\LFKom\\Downloads\\image_2023-10-24_19-32-46.png";
+    public static String inputImageFolderPath;
+    public static String tempFolderPath;
     public static String grayscaleImagePath;
     public static String tempImagePath;
     public static void main(String[] args) throws TesseractException, IOException {
 
-        int lastStop = imagePath.lastIndexOf('.');
-        if (lastStop >= 0) {
-            grayscaleImagePath = imagePath.substring(0, lastStop) + "_grayscale.png";
+        inputImageFolderPath = new File("C:\\Users\\LFKom\\Downloads\\image_2023-10-24_19-32-46.png").getParentFile().toString();
+        tempFolderPath = new File("C:\\Users\\LFKom\\Downloads\\image_2023-10-24_19-32-46.png").getParentFile().toString() + File.separator + "tmp";
+
+        if (!new File(tempFolderPath).exists()) {
+            if (!new File(tempFolderPath).mkdirs()) {
+                System.out.println("Failed to create tmp directory");
+            }
         }
 
-        int lastSlash = imagePath.lastIndexOf(File.separator);
-        if (lastSlash >= 0) {
-            folderPath = imagePath.substring(0, lastSlash) + File.separator;
-            tempImagePath = folderPath + "tempimage.png";
-        }
+        grayscaleImagePath = tempFolderPath + "input_grayscale.png";
+        tempImagePath = tempFolderPath + "tempimage.png";
 
-        BufferedImage inputImage = loadImage(imagePath);
-        BufferedImage grayscaleImage = imageToGrayscale(inputImage, grayscaleImagePath);
-        setImageThreshold(grayscaleImage, tempImagePath, 170);
-        BufferedImage tempImage = loadImage(tempImagePath);
-
-        int[] firstQuestCoords = findFirstQuest(tempImage);
-
-
-        int remainingHeight = inputImage.getHeight() - firstQuestCoords[1];
-        int questsCount = remainingHeight / ((firstQuestCoords[3] - firstQuestCoords[1]) + 6);
-
-        int heightIncrement = (firstQuestCoords[3] - firstQuestCoords[1]) + 6;
-
-        int i = 0;
-        while (i < questsCount + 1) {
-            cropImageByPixels(inputImage, folderPath + "tempimage" + i + ".png", firstQuestCoords[0], firstQuestCoords[1], firstQuestCoords[2], firstQuestCoords[3]);
-            System.out.println("i = " + i + "; firstQuestCoords[1] = " + firstQuestCoords[1] + "; firstQuestCoords[3] = " + firstQuestCoords[3]);
-            firstQuestCoords[1] += heightIncrement;
-            firstQuestCoords[3] += heightIncrement;
-            i++;
-        }
-        System.out.println(inputImage.getHeight());
-        //tempImage = cropImageByPixels(inputImage, tempImagePath, firstQuestCoords[0], firstQuestCoords[1], firstQuestCoords[2], firstQuestCoords[3]);
+//
+//        BufferedImage inputImage = loadImage(inputImagePath);
+//        BufferedImage grayscaleImage = imageToGrayscale(inputImage, grayscaleImagePath);
+//        setImageThreshold(grayscaleImage, tempImagePath, 170);
+//        BufferedImage tempImage = loadImage(tempImagePath);
+//
+//        int[] firstQuestCoords = findFirstQuest(tempImage);
+//
+//
+//        int remainingHeight = inputImage.getHeight() - firstQuestCoords[1];
+//        int questsCount = remainingHeight / ((firstQuestCoords[3] - firstQuestCoords[1]) + 6);
+//
+//        int heightIncrement = (firstQuestCoords[3] - firstQuestCoords[1]) + 6;
+//
+//        int i = 0;
+//        while (i < questsCount + 1) {
+//            cropImageByPixels(inputImage, inputImageFolderPath + "tempimage" + i + ".png", firstQuestCoords[0], firstQuestCoords[1], firstQuestCoords[2], firstQuestCoords[3]);
+//            System.out.println("i = " + i + "; firstQuestCoords[1] = " + firstQuestCoords[1] + "; firstQuestCoords[3] = " + firstQuestCoords[3]);
+//            firstQuestCoords[1] += heightIncrement;
+//            firstQuestCoords[3] += heightIncrement;
+//            i++;
+//        }
+//        System.out.println(inputImage.getHeight());
 
 
 
