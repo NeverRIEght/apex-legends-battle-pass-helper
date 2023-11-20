6adf89b38d6a26dd214a274b3e13b0a2164335cb
Mihail Komarov <michaelkomarov03@gmail.com>
2023-11-20 18:58:42 +0200

    UI-Design: sidebar buttons

--- src/main/java/org/apexlegendsbphelper/App.java
+++ src/main/java/org/apexlegendsbphelper/App.java
@@ -16,24 +16,25 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(new File("D:\\Java\\apex-legends-battle-pass-helper\\src\\main\\java\\org\\apexlegendsbphelper\\View\\main-page.fxml").toURI().toURL());
-//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 850, 478);
 
 
-        Group root = new Group();
-        Scene scene = new Scene(root, 850, 478);
-        stage.setTitle("Hello!");
-
-
-        File file = new File("D:\\Java\\apex-legends-battle-pass-helper\\src\\main\\java\\org\\apexlegendsbphelper\\ignite_poster.jpg");
-        Image image = new Image(file.toURI().toURL().toString());
-        ImageView imageView = new ImageView(image);
-        imageView.setX(0);
-        imageView.setY(0);
-        imageView.fitWidthProperty().bind(scene.widthProperty());
-        imageView.fitHeightProperty().bind(scene.heightProperty());
-
-
-        root.getChildren().add(imageView);
+//        Group root = new Group();
+//        Scene scene = new Scene(root, 850, 478);
+//        stage.setTitle("Hello!");
+//
+//
+//        File file = new File("D:\\Java\\apex-legends-battle-pass-helper\\src\\main\\java\\org\\apexlegendsbphelper\\ignite_poster.jpg");
+//        Image image = new Image(file.toURI().toURL().toString());
+//        ImageView imageView = new ImageView(image);
+//        imageView.setX(0);
+//        imageView.setY(0);
+//        imageView.fitWidthProperty().bind(scene.widthProperty());
+//        imageView.fitHeightProperty().bind(scene.heightProperty());
+//
+//
+//        root.getChildren().add(imageView);
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
