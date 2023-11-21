edc407f0650eff0eac0269129c2f93fe09808d5e
Mihail Komarov <michaelkomarov03@gmail.com>
2023-11-21 17:27:47 +0200

    Import classes

---
+++ src/main/java/org/apexlegendsbphelper/Model/FileUtil.java
@@ -0,0 +1,43 @@
+package org.apexlegendsbphelper.Model;
+
+import java.io.File;
+
+public class FileUtil {
+    public static boolean deleteDirectory(File directory) {
+        if (directory.exists() && directory.isDirectory()) {
+            File[] files = directory.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isDirectory()) {
+                        if (!deleteDirectory(file)) {
+                            return false;
+                        }
+                    } else {
+                        if (!file.delete()) {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return directory.delete();
+        }
+        return false;
+    }
+
+    public static void recreateDirectories(String tempFolderPath) {
+
+        if (new File(tempFolderPath).exists()) {
+            deleteDirectory(new File(tempFolderPath));
+        }
+
+        new File(tempFolderPath).mkdirs();
+        new File(tempFolderPath + File.separator + "tmpQuests").mkdirs();
+        new File(tempFolderPath + File.separator + "tmpQuestsBR").mkdirs();
+        new File(tempFolderPath + File.separator + "tmpQuestsNBR").mkdirs();
+        new File(tempFolderPath + File.separator + "tmpQuestsReward").mkdirs();
+        new File(tempFolderPath + File.separator + "tmpQuestsReg").mkdirs();
+        new File(tempFolderPath + File.separator + "tmpQuestsBRProgress").mkdirs();
+        new File(tempFolderPath + File.separator + "tmpQuestsNBRProgress").mkdirs();
+        new File(tempFolderPath + File.separator + "tmpQuestsRegProgress").mkdirs();
+    }
+}
