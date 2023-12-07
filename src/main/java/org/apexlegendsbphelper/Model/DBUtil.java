79cafce931b85457c8129e31f9dee6e0eca83d1b
Mihail Komarov <michaelkomarov03@gmail.com>
2023-12-07 15:27:55 +0200

    Example of database connection via StringBuilder

--- src/main/java/org/apexlegendsbphelper/Model/DBUtil.java
+++ src/main/java/org/apexlegendsbphelper/Model/DBUtil.java
@@ -1,12 +1,21 @@
 package org.apexlegendsbphelper.Model;
+import java.io.File;
 import java.sql.*;
 
 public class DBUtil {
-    public static void main(String[] a)
-            throws Exception {
-        Connection conn = DriverManager.
-                getConnection("jdbc:h2:file:" + System.getProperty("user.dir") + "/data/apexhelperdb", "sa", "");
+    public static void main(String[] a) throws Exception {
+        StringBuilder dbUrlBuilder = new StringBuilder();
+        dbUrlBuilder.append("jdbc:h2:file:");
+        dbUrlBuilder.append(System.getProperty("user.dir"));
+        dbUrlBuilder.append(File.separator);
+        dbUrlBuilder.append("data");
+        dbUrlBuilder.append(File.separator);
+        dbUrlBuilder.append("apexlegendsbphelper");
+
+        Connection conn = DriverManager.getConnection(dbUrlBuilder.toString(), "sa", "");
+
         // add application code here
+
         conn.close();
     }
 }
