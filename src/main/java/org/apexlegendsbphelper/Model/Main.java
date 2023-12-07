cedd2253db93817a9ffbc22b9a55d94efbb3ba06
Mihail Komarov <michaelkomarov03@gmail.com>
2023-12-07 12:45:08 +0200

    Add h2 files

--- src/main/java/org/apexlegendsbphelper/Model/Main.java
+++ src/main/java/org/apexlegendsbphelper/Model/Main.java
@@ -9,8 +9,8 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws TesseractException, IOException {
         recreateTempDirectories();
-        Quest[] quests1 = processWeekImages("D:\\apex-tests\\testdata1.png", "D:\\apex-tests\\testdata2.png");
-//        Quest[] quests1 = processWeekImages("/Users/michaelkomarov/Downloads/image_2023-11-22_10-50-14.png", "/Users/michaelkomarov/Downloads/image_2023-11-22_10-50-32.png");
+//        Quest[] quests1 = processWeekImages("D:\\apex-tests\\testdata1.png", "D:\\apex-tests\\testdata2.png");
+        Quest[] quests1 = processWeekImages("/Users/michaelkomarov/Downloads/image_2023-11-22_10-50-14.png", "/Users/michaelkomarov/Downloads/image_2023-11-22_10-50-32.png");
 
         for (Quest quest : quests1) {
             System.out.println(quest.getQuestNameBR());
