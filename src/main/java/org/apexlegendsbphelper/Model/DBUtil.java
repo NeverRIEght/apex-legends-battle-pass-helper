47fe2cb82c8224ada4769bc2ffb16969b085a9d3
Mihail Komarov <michaelkomarov03@gmail.com>
2023-12-07 17:41:55 +0200

    wipeDB

--- src/main/java/org/apexlegendsbphelper/Model/DBUtil.java
+++ src/main/java/org/apexlegendsbphelper/Model/DBUtil.java
@@ -4,19 +4,19 @@ import java.sql.*;
 
 public class DBUtil {
     public static void main(String[] a) throws Exception {
-        if(createDB()) {
-            Connection dbConnection = openDBConnection();
-            Statement stmt = dbConnection.createStatement();
-            String query =
-                    "INSERT INTO Quests (questID, weekNumber, questNameBR, questNameNBR, isCompleted, questReward)" +
-                            "VALUES" +
-                            "(1, 5, 'Поиск сокровищ', 'Treasure Hunt', true, 8)," +
-                            "(2, 8, 'Спасение затонувшего корабля', 'Rescue Sunken Ship', false, 5)," +
-                            "(3, 3, 'Путешествие в древний лес', 'Journey to Ancient Forest', true, 10);";
+        wipeDB();
+        createDB();
+        Connection dbConnection = openDBConnection();
+        Statement stmt = dbConnection.createStatement();
+        String query =
+                "INSERT INTO Quests (questID, weekNumber, questNameBR, questNameNBR, isCompleted, questReward)" +
+                        "VALUES" +
+                        "(1, 5, 'Поиск сокровищ', 'Treasure Hunt', true, 8)," +
+                        "(2, 8, 'Спасение затонувшего корабля', 'Rescue Sunken Ship', false, 5)," +
+                        "(3, 3, 'Путешествие в древний лес', 'Journey to Ancient Forest', true, 10);";
 
-            stmt.executeUpdate(query);
-            closeDBConnection(dbConnection);
-        }
+        stmt.executeUpdate(query);
+        closeDBConnection(dbConnection);
     }
 
     public static Connection openDBConnection() throws SQLException {
@@ -36,39 +36,54 @@ public class DBUtil {
             throw e;
         }
     }
-    public static boolean closeDBConnection(Connection dbConnection) throws SQLException {
+    public static void closeDBConnection(Connection dbConnection) throws SQLException {
         try {
             dbConnection.close();
-            return true;
         } catch (SQLException e) {
             System.out.println("Error while closing DB connection: " + e.getMessage());
-            return false;
+            throw e;
         }
     }
-    public static boolean createDB() throws SQLException {
+    public static void createDB() throws SQLException {
         try {
             Connection dbConnection = openDBConnection();
             Statement stmt = dbConnection.createStatement();
 
             String query =
-                    "DROP TABLE IF EXISTS Quests; " +
-                            "CREATE TABLE Quests" +
-                            "(" +
-                            "questID INTEGER UNIQUE not NULL, " +
-                            "weekNumber TINYINT NOT NULL CHECK (weekNumber > 0 AND weekNumber <= 12), " +
-                            "questNameBR VARCHAR(255) NOT NULL, " +
-                            "questNameNBR VARCHAR(255), " +
-                            "isCompleted BOOLEAN, " +
-                            "questReward TINYINT CHECK (questReward > 0 AND questReward <= 10), " +
-                            "PRIMARY KEY (questID)" +
-                            ")";
+                    "CREATE TABLE IF NOT EXISTS Quests" +
+                    "(" +
+                    "questID INTEGER UNIQUE not NULL, " +
+                    "weekNumber TINYINT NOT NULL CHECK (weekNumber > 0 AND weekNumber <= 12), " +
+                    "questNameBR VARCHAR(255) NOT NULL, " +
+                    "questNameNBR VARCHAR(255), " +
+                    "isCompleted BOOLEAN, " +
+                    "questReward TINYINT CHECK (questReward > 0 AND questReward <= 10), " +
+                    "PRIMARY KEY (questID)" +
+                    ")";
 
             stmt.executeUpdate(query);
             closeDBConnection(dbConnection);
-            return true;
         } catch (SQLException e) {
             System.out.println("Error while creating DB: " + e.getMessage());
             throw e;
         }
     }
+    public static void wipeDB() throws SQLException {
+        try {
+            Connection dbConnection = openDBConnection();
+            Statement stmt = dbConnection.createStatement();
+
+            String query =
+                    "DROP TABLE IF EXISTS Quests;";
+
+            stmt.executeUpdate(query);
+            closeDBConnection(dbConnection);
+        } catch (SQLException e) {
+            System.out.println("Error while wiping DB: " + e.getMessage());
+            throw e;
+        }
+    }
+//    public static void addQuestToDB(Quest inputQuest) {
+//
+//    }
 }
