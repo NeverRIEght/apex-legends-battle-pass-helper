0fb608254ed887cc04b6b662cf2fcc6b7c28749c
Mihail Komarov <michaelkomarov03@gmail.com>
2023-12-07 15:10:15 +0200

    Example of database connection

--- src/main/java/org/apexlegendsbphelper/Model/DBUtil.java
+++ src/main/java/org/apexlegendsbphelper/Model/DBUtil.java
@@ -5,7 +5,7 @@ public class DBUtil {
     public static void main(String[] a)
             throws Exception {
         Connection conn = DriverManager.
-                getConnection("jdbc:h2:file:../../../data/apexhelperdb", "sa", "");
+                getConnection("jdbc:h2:file:" + System.getProperty("user.dir") + "/data/apexhelperdb", "sa", "");
         // add application code here
         conn.close();
     }
